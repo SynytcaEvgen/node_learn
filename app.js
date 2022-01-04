08436e62826194366b5d95ad4d04f57959d22078
Evgen Synytsia <evgen@Yuliias-MacBook-Air.local>
2022-01-04 08:13:14 +0200

    next com

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const fs = require('fs');
-const { characters, stealRing } = require('./characters');
+// const fs = require('fs');
+// const { characters, stealRing } = require('./characters');
 
 
-const data = fs.readFileSync('./data.txt');
+// const data = fs.readFileSync('./data.txt');
 
-newChar = stealRing(characters, 'Bilbo')
+// newChar = stealRing(characters, 'Bilbo')
 
-console.log(characters);
-console.log(newChar);
+// console.log(characters);
+// console.log(newChar);
+
+console.log(__dirname);
