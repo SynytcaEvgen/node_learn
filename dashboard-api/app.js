f70ded948bb9d777068521b4699f4cdc6d3d4632
Synytsia Yevhen <yevhen.synytsia@niko-tech.eu>
2022-01-13 11:40:57 +0200

    initial commit

---
+++ dashboard-api/app.js
@@ -0,0 +1,24 @@
+import http from 'http';
+import express from 'express';
+
+
+const port = 5000;
+const app = express();
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello mudila!')
+// })
+
+// server.listen(port, host, () => {
+//     console.log(`Server run to ${host}:${port}`);
+// })
+
+app.get('/hello', (req, res) => {
+    res.send('Hello mudila it is Express')
+})
+
+app.listen(port, () => {
+    console.log(`Server run to port - ${port}`);
+})
